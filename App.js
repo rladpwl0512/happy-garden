8ebd67bde06370147cf02b27e9039ec731991908
KIM YEJI <rladpwl0512@gmail.com>
2023-04-07 15:48:55 +0900

    refactor: 필요없는 코드 삭제

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native";
-import colors from "./styles/theme";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
@@ -11,38 +9,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Welcome" }}
-        />
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="MoodJournal" component={MoodJournalScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  writeDiaryButton: {
-    alignItems: "center",
-    justifyContent: "center",
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    backgroundColor: colors.PRIMARY_100,
-    shadowColor: colors.BLACK,
-    shadowOffset: {
-      width: 0,
-      height: 7,
-    },
-    shadowOpacity: 0.21,
-    shadowRadius: 7.68,
-    elevation: 10,
-  },
-});
