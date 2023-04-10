c68e6728ac8d5e1a09bdba8e7b6c9dd5308548c7
KIM YEJI <rladpwl0512@gmail.com>
2023-04-10 16:08:48 +0900

    refactor: App.js 리팩터링
    - import 문 정렬
    - 변수명 변경 (isReady -> isReadyFont)
    - 초기 화면 원상복귀

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
+import { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppLoading from "expo-app-loading"; //TODO: deprecated. expo-splash-screen 사용해서 변경하기
+import * as Font from "expo-font";
 import HomeScreen from "./screens/HomeScreen";
 import MoodJournalScreen from "./screens/MoodJournalScreen";
 import ThanksJournalScreen from "./screens/ThanksJournalScreen";
 import JournalFeedbackScreen from "./screens/JournalFeedbackScreen";
 
-import * as Font from "expo-font";
-import { useState } from "react";
-import AppLoading from "expo-app-loading"; //TODO: deprecated. expo-splash-screen 사용해서 변경하기
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [isReady, setIsReady] = useState(false);
+  const [isReadyFont, setIsReadyFont] = useState(false);
 
   const getFonts = async () => {
     await Font.loadAsync({
@@ -21,11 +20,11 @@ export default function App() {
     });
   };
 
-  return isReady ? (
+  return isReadyFont ? (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{ headerShown: false }}
-        initialRouteName="ThanksJournal"
+        initialRouteName="Home"
       >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="MoodJournal" component={MoodJournalScreen} />
@@ -39,8 +38,8 @@ export default function App() {
   ) : (
     <AppLoading
       startAsync={getFonts}
-      onFinish={() => setIsReady(true)}
-      onError={() => {}}
+      onFinish={() => setIsReadyFont(true)}
+      onError={() => {}} // TODO: 로딩에 에러가 났을 때, 대응 추가
     />
   );
 }
